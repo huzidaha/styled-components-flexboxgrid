a25a342adc8286f29a03ac3d0208d2903c94be5e
huzidaha <huzidaha@126.com>
2017-03-04 22:57:21 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
 # styled-components-flexboxgrid
-flexboxgrid in styled-components
+
+(NOTE: This project is in processing.)
+
+Grid layout system base on [styled-components](https://github.com/styled-components/styled-components) and inspired by [Flexbox Grid](http://flexboxgrid.com/).
+
+`styled-components-flexboxgrid` uses [styled-components](https://github.com/styled-components/styled-components) as peer dependency. Please confirm that you have styled-components installed in your project.
+
+## Install
+
+```
+npm install styled-components-flexboxgrid --save
+```
+
+## Basic Usage
+
+```
+import React, { Component } from 'react';
+import { Col, Row } from 'styled-components-flexboxgrid'
+
+class App extends Component {
+  render() {
+    return (
+      <Row>
+        <Col xs={1} md={6} lg={12}>666</Col>
+        <Col xs={1} md={6} lg={12}>777</Col>
+      </Row>
+    )
+  }
+}
+```
+
+## License
+MIT License
+
+Copyright (c) 2017 胡子大哈
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ src/index.js
@@ -0,0 +1,105 @@
+import styled from 'styled-components'
+
+const rem = (num) => `${num}rem`
+
+const gutterWidth = 1
+const outerMargin = 2
+const gutterCompensation = gutterWidth * -0.5
+const halfGutterWidth = gutterWidth * 0.5
+const xsMin = 30
+const smMin = 48
+const mdMin = 64
+const lgMin = 75
+const containerSm = smMin + gutterWidth
+const containerMd = mdMin + gutterWidth
+const containerLg = lgMin + gutterWidth
+
+const smViewport = 'only screen and (min-width: 48em)'
+const mdViewport = 'only screen and (min-width: 64em)'
+const lgViewport = 'only screen and (min-width: 75em)'
+
+const CommonContainer = styled.div`
+  margin-right: auto;
+  margin-left: auto;
+`
+
+export const ContainerFluid = styled(CommonContainer)`
+  padding-right: var(--outer-margin, 2rem);
+  padding-left: var(--outer-margin, 2rem);
+`
+
+export const Container = styled(CommonContainer)`
+  @media ${smViewport} {
+    width: ${rem(containerSm)};
+  }
+  @media ${mdViewport} {
+    width: ${rem(containerMd)};
+  }
+  @media ${lgViewport} {
+    width: ${rem(containerLg)};
+  }
+`
+
+export const Row = styled.div`
+  box-sizing: border-box;
+  display: flex;
+  flex: 0 1 auto;
+  flex-direction: row;
+  flex-wrap: wrap;
+  margin-right: ${rem(gutterCompensation)};
+  margin-left: ${rem(gutterCompensation)};
+  flex: ${props => props.reverse ? 'row-reverse' : 'row'};
+`
+
+const widths = [
+  '8.33333333%',
+  '16.66666667%',
+  '25%',
+  '33.33333333%',
+  '41.66666667%',
+  '50%',
+  '58.33333333%',
+  '66.66666667%',
+  '75%',
+  '83.33333333%',
+  '91.66666667%',
+  '100%'
+]
+
+const generateWidthByProps = (key, props, isMaxWidth = false) => {
+  const partition = props[key]
+  const width = widths[partition - 1]
+  return width || (isMaxWidth ? '100%' : '0')
+}
+
+const generateMediaQueryForCol = (viewPortKey, props) => {
+  const viewPorts = {
+    lg: lgViewport,
+    md: mdViewport,
+    sm: smViewport
+  }
+  return !props[viewPortKey] ? '' : `
+    @media ${viewPorts[viewPortKey]} {
+      flex-basis: ${generateWidthByProps(viewPortKey, props)};
+      max-width: ${generateWidthByProps(viewPortKey, props, true)};
+    }
+  `
+}
+
+export const Col = styled.div`
+  box-sizing: border-box;
+  flex: 0 0 auto;
+  padding-right: ${rem(halfGutterWidth)};
+  padding-left: ${rem(halfGutterWidth)};
+  flex-direction: ${props => props.reverse ? 'column-reverse' : 'column'};
+  flex-grow: 1;
+  flex-basis: ${props => generateWidthByProps('xs', props)};
+  max-width: ${props => generateWidthByProps('xs', props, true)};
+  ${props => {
+    return `
+      ${generateMediaQueryForCol('sm', props)}
+      ${generateMediaQueryForCol('md', props)}
+      ${generateMediaQueryForCol('lg', props)}
+    `
+  }}
+`
